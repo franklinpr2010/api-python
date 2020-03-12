6b3ed145e2768e5f9d25af52f11351521ba04531
franklin.roza <franklinpr2010@gmail.com>
2020-03-11 22:50:10 -0300

    oneToOne

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -1,18 +1,62 @@
 from rest_framework.fields import SerializerMethodField
 from rest_framework.serializers import ModelSerializer
-from core.models import PontoTuristico
+
+from atracoes.models import Atracao
+from core.models import PontoTuristico, DocIdentificacao
 from core.models import PontoTuristico
 from atracoes.api.serializers import AtracoesSerializer
+from enderecos.models import Endereco
+
+
+class DocIndetificacaoSerializer(ModelSerializer):
+    class Meta:
+        model: DocIdentificacao
+        #mostra todos os filtros
+        fields = '__all__'
 
 
 # Serializers define the API representation.
 class PontoTuristicoSerializer(ModelSerializer):
     atracoes = AtracoesSerializer(many=True)
     descricao_completa = SerializerMethodField()
+    doc_identificacao = DocIndetificacaoSerializer()
     class Meta:
         model = PontoTuristico
         fields = ['id', 'nome', 'descricao', 'aprovado', 'foto' , 'atracoes', 'comentarios', 'avaliacoes', 'endereco',
-                  'descricao_completa', 'get_descricao_completa2']
+                  'descricao_completa', 'get_descricao_completa2', 'doc_identificacao']
 
     def get_descricao_completa(self, obj):
-        return 's% - %s' %(obj.nome, obj.descricao)
+        return'%s - %s' %  {obj.nome} - {obj.descricao}
+
+
+    def cria_atracoes(self, atracoes, ponto):
+        for atracao in atracoes:
+            at = Atracao.objects.create(**atracao)
+            ponto.atracoes.all(at)
+
+    '''
+    
+    '''
+    def create(self, validated_data):
+        #atracao
+        atracoes = validated_data['atracoes']
+        del validated_data['atracoes']
+        ponto = PontoTuristico.objects.create(**validated_data)
+        self.cria_atracoes(atracoes, ponto)
+
+        #doc_identificacao
+        doc = validated_data['doc_identificacao']
+        del validated_data['doc_identificacao']
+        doci = DocIdentificacao.objects.create(**doc)
+        ponto.doc_identificacao = doc
+
+        #endereco
+        endereco = validated_data['endereco']
+        del validated_data['endereco']
+        end = Endereco.objects.create(**endereco)
+        ponto.endereco = end
+
+        ponto.save()
+        return ponto
+
+
